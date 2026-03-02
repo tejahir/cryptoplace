4d044677dd695dbb2ad680588c8203d73985009e
tejahir <ladumortejas682@gmail.com>
2026-03-02 10:54:28 +0530

    fix SignUp import path

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
 import Footer from './components/Navbar/Footer/Footer'
-import SignUp from './components/signup/SignUP'
+import SignUp from './components/signup/SignUp'
 import SignIn from './components/signin/SignIn'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/coin/:coinId' element={<Coin/>}/>
-        <Route path='/SignUp' element={<SignUp/>}/>
-        <Route path='/SignIn' element={<SignIn/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/coin/:coinId' element={<Coin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
